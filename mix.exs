3580f736cd13b709c4535b4ca4c76ab1d2b44283
Johnson Denen <johnson.denen@gmail.com>
2018-12-12 11:16:36 -0500

    Fill in basic package details

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule Crucible.MixProject do
   def project do
     [
       app: :crucible,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -21,8 +23,19 @@ defmodule Crucible.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Johnson Denen"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jdenen/crucible"}
+    ]
+  end
+
+  defp description do
+    "TODO"
   end
 end
